f480cf5745a7a3295fc402985723be6b886a60f3
Ricardo Cordova <tchakinha2504@hotmail.com>
2023-02-23 10:33:15 -0300

    Update index.js com todos os testes passando

--- src/index.js
+++ src/index.js
@@ -10,19 +10,68 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find((user) => user.username === username)
+
+  if (!user) {
+    return response.status(404).json({ error: "Usuário não existe"})
+  }
+
+  request.user = user
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if (user.todos.length === 10 && user.pro === false) {
+    return response.status(403).json({ error: "Atualiza para o plano pro"})
+  }
+
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  const user = users.find((user) => user.username === username)
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: "O id não é válido"})
+  }
+
+  if (!user) {
+    return response.status(404).json({ error: "Usuário não encontrado"})
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id)
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo não encontrado"})
+  }
+
+  request.user = user
+  request.todo = todo
+
+  return next()
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user =  users.find((user) => user.id === id)
+
+  if (!user) {
+    return response.status(404).json({error: "Usuário não encontrado"})
+  }
+
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
